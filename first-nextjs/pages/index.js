f34da67ae532a40ff4825d91ddc08caa7150949e
BibliyaSeo <seongkyeong.seo@gmail.com>
2022-10-14 18:21:51 +0900

    Add: first-nextjs

---
+++ first-nextjs/pages/index.js
@@ -0,0 +1,60 @@
+// import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState();
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
+
+  return (
+    <div className="container">
+      <Seo title={"Home"} />
+      {/* {!movies && <h4>Loading...</h4>} */}
+      {results?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  // 함수 이름 바꾸면 안 됨!
+  // 여기에 쓰는 코드는 클라이언트에 보이지 않는다
+  // rewrites에 말고 여기에 API_KEY를 써도 된다
+
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
